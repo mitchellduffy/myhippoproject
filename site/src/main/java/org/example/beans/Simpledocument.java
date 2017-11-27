c505ba34c1f3ed14edaf37c1858fa91224e63680
Mitchell Duffy <mitchellduffy@mitchelsmacbook.speedport_w_723v_1_45_000>
2017-11-27 18:58:28 +0100

    Finished the Hello World Tutorial

---
+++ site/src/main/java/org/example/beans/Simpledocument.java
@@ -0,0 +1,19 @@
+package org.example.beans;
+
+import org.onehippo.cms7.essentials.dashboard.annotations.HippoEssentialsGenerated;
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoHtml;
+
+@HippoEssentialsGenerated(internalName = "myhippoproject:simpledocument")
+@Node(jcrType = "myhippoproject:simpledocument")
+public class Simpledocument extends BaseDocument {
+    @HippoEssentialsGenerated(internalName = "myhippoproject:title")
+    public String getTitle() {
+        return getProperty("myhippoproject:title");
+    }
+
+    @HippoEssentialsGenerated(internalName = "myhippoproject:content")
+    public HippoHtml getContent() {
+        return getHippoHtml("myhippoproject:content");
+    }
+}
